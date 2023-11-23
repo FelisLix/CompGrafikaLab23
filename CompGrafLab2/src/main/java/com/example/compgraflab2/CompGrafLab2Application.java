486d55f14f6ffc20db24823ce5821ee82b9c7628
FelisLix <126716323+FelisLix@users.noreply.github.com>
2023-11-23 14:41:38 +0200

    Add files via upload

---
+++ CompGrafLab2/src/main/java/com/example/compgraflab2/CompGrafLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.compgraflab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompGrafLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompGrafLab2Application.class, args);
+    }
+
+}
---
+++ CompGrafLab2/src/test/java/com/example/compgraflab2/CompGrafLab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.compgraflab2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompGrafLab2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
